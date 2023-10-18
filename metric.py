0521094ad94e83939401addbf338241210c3dc4a
Minseok Choe <choems@outlook.com>
2023-10-19 02:04:44 +0900

    edit metric.py

--- metric.py
+++ metric.py
@@ -1,5 +1,6 @@
 import torch
 
+
 def quadrant(class_index, predict, truth):
     c = class_index
     true_p = torch.where((c == truth) & (c == predict), 1, 0).sum()  # true positive for c
@@ -7,7 +8,6 @@ def quadrant(class_index, predict, truth):
     false_n = torch.where((c == truth) & (c != predict), 1, 0).sum()  # false negative for c
     true_n = torch.where((c != truth) & (c != predict), 1, 0).sum()  # true negative for c
     return {"true_p": true_p, "false_p": false_p, "false_n": false_n, "true_n": true_n}
-
 
 def precision_micro(num_classes, predict, truth):
     true_p_of_all_c = 0
@@ -23,18 +23,10 @@ def precision_macro(num_classes, predict, truth):
     sum_of_precision_of_class_c = 0
     for c in range(num_classes):
         quad = quadrant(class_index=c, predict=predict, truth=truth)
-        sum_of_precision_of_class_c += quad["true_p"] / (quad["true_p"] + quad["false_p"])
+        precision_of_class_c = quad["true_p"] / (quad["true_p"] + quad["false_p"])
+        sum_of_precision_of_class_c += precision_of_class_c if (
+                    precision_of_class_c == precision_of_class_c) else 0  # NaN Check
     return sum_of_precision_of_class_c / num_classes
-
-
-def precision_weight(num_classes, predict, truth):
-    weighted_sum_of_precision_of_class_c = 0
-    for c in range(num_classes):
-        quad = quadrant(class_index=c, predict=predict, truth=truth)
-        number_of_c_in_truth = torch.where(truth == c, 1, 0).sum(dtype=torch.float64)
-        weighted_sum_of_precision_of_class_c += (quad["true_p"] / (
-                quad["true_p"] + quad["false_p"])) * number_of_c_in_truth / num_classes
-    return weighted_sum_of_precision_of_class_c
 
 
 def recall_micro(num_classes, predict, truth):
@@ -51,20 +43,16 @@ def recall_macro(num_classes, predict, truth):
     sum_of_recall_of_class_c = 0
     for c in range(num_classes):
         quad = quadrant(class_index=c, predict=predict, truth=truth)
-        sum_of_recall_of_class_c += quad["true_p"] / (quad["true_p"] + quad["false_n"])
+        recall_of_class_c = quad["true_p"] / (quad["true_p"] + quad["false_n"])
+        sum_of_recall_of_class_c += recall_of_class_c if (recall_of_class_c == recall_of_class_c) else 0  # NaN check
     return sum_of_recall_of_class_c / num_classes
 
 
-def recall_weight(num_classes, predict, truth):
-    weighted_sum_of_recall_of_class_c = 0
-    for c in range(num_classes):
-        quad = quadrant(class_index=c, predict=predict, truth=truth)
-        number_of_c_in_truth = torch.where(truth == c, 1, 0).sum(dtype=torch.float64)
-        weighted_sum_of_recall_of_class_c += (quad["true_p"] / (
-                quad["true_p"] + quad["false_n"])) * number_of_c_in_truth / num_classes
-    return weighted_sum_of_recall_of_class_c
-
+# it gets "one-hot vector" version of "truth" and "predict"
 def f1score(num_classes, predict, truth, precision, recall):
-    pval = precision(num_classes=num_classes, predict=predict, truth=truth)
-    rval = recall(num_classes=num_classes, predict=predict, truth=truth)
+    predict_index = torch.tensor([torch.argmax(predict[i]) for i in range(predict.size()[0])])
+    truth_index = torch.tensor([torch.argmax(truth[i]) for i in range(truth.size()[0])])
+    print("predict_index", predict_index)
+    pval = precision(num_classes=num_classes, predict=predict_index, truth=truth_index)
+    rval = recall(num_classes=num_classes, predict=predict_index, truth=truth_index)
     return 2 * (pval * rval) / (pval + rval)
