eb23f40cebc55d9bc7f5cf2d096e8a67efd3cb81
Minseok Choe <choems@outlook.com>
2023-10-19 13:59:01 +0900

    edit svm.py to correctly calculate loss

--- svm.py
+++ svm.py
@@ -10,43 +10,54 @@ def _one_hot(n, k):
     tensor[k] = 1
     return tensor
 
+
 def _one_hot_list(n, k):
-    # Create a tensor of zeros of size n
     tensor = n * [0]
-
-    # Set the k-th dimension to 1
     tensor[k] = 1
-
     return tensor
-
 
 
 class KernelSVM(nn.Module):
     # f should be a function from torch : number of feature -> torch : new space.
-    def __init__(self, input_dim, num_classes, kernel=(lambda x: x), C=1.0):
+    def __init__(self, input_dim, num_classes, kernel=(lambda x: x), C=1.0, margin=1.0):
         super(KernelSVM, self).__init__()
         self.weight = nn.Parameter(torch.randn((input_dim, num_classes), requires_grad=True))
         self.bias = nn.Parameter(torch.randn((1, num_classes), requires_grad=True))
         self.C = C
-        self.kernelToMatrix = lambda x : torch.stack([kernel(x[i]) for i in range(x.size()[0])])
+        self.kernelToMatrix = lambda x: torch.stack([kernel(x[i]) for i in range(x.size()[0])])
         self.num_classes = num_classes
-        self.optimizer = optim.SGD(self.parameters(), lr=0.01)
+        self.margin = margin
 
     def train(self, X, y, epochs=100):
-        kernel_X = self.kernelToMatrix(X)
+        kernel_x = self.kernelToMatrix(X)
+        optimizer = optim.SGD(self.parameters(), lr=0.01)
         for epoch in range(epochs):
-            self.optimizer.zero_grad()
-            # binary_labels ∈ {+1, -1}
-            loss = self(kernel_X, y)
+            optimizer.zero_grad()
+            loss = self(kernel_x, y)
             loss.backward()
-            self.optimizer.step()
+            optimizer.step()
 
-    def forward(self, X, y): ## 여기서는 kernel을 적용하지 않음 - 이미 적용되어있음
-        output = torch.matmul(X, self.weight) + self.bias
-        # Hinge loss
-        loss = 0.5 * self.weight.pow(2).sum() + self.C * torch.clamp(1 - y * output, min=0).sum()
+    def multiclass_hinge_loss_one_hot(self, outputs, y_one_hot):
+        correct_class_scores = (outputs * y_one_hot).sum(1)
+        margins = torch.clamp(outputs - correct_class_scores.unsqueeze(1) + self.margin, min=0)
+        margins = margins * (1 - y_one_hot)  # Zero-ing for correct class
+        loss = margins.sum(1).mean()
         return loss
 
-    def predict(self, x):
+    def forward(self, X, y):
+        output = torch.matmul(X, self.weight) + self.bias
+        hinge_loss = self.multiclass_hinge_loss_one_hot(output, y)
+        reg_loss = 0.5 * self.weight.pow(2).sum()
+        total_loss = reg_loss + self.C * hinge_loss
+        return total_loss
+
+    def predict_onehot(self, x):
         output = torch.matmul(self.kernelToMatrix(x), self.weight) + self.bias
         return torch.stack([_one_hot(self.num_classes, i) for i in torch.argmax(output, dim=1)])
+
+    def predict_index(self, x):
+        output = torch.matmul(self.kernelToMatrix(x), self.weight) + self.bias
+        return torch.argmax(output, dim=1)
+
+    def predict(self, x, using=predict_onehot):
+        return using(self, x)
